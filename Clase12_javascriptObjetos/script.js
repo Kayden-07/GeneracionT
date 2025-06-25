d1d0d274b0a2ad528c0ac6cc50b3f1878e416485
Gino Mazzaglia <mazzagliagino@gmail.com>
2025-06-25 16:59:18 -0300

    empieza clase 12

--- Clase12_javascriptObjetos/script.js
+++ Clase12_javascriptObjetos/script.js
@@ -1,13 +1,56 @@
-//Objetos
-//CLAVE: EL NOMBRE DE UNA PROPIEDAD
-//VALOR: CONTENIDO ASOCIADO A LA CLAVE
+// //Objetos
+// //CLAVE: EL NOMBRE DE UNA PROPIEDAD
+// //VALOR: CONTENIDO ASOCIADO A LA CLAVE
+// let persona = {
+//     nombre: "Agus",
+//     edad: 31,
+//     personaje: "Power ranger rosa",
+//     saludar: function () {
+//         console.log("Hola soy ", this.nombre)
+//     }
+// }
+
+// persona.saludar()
+
+// let personas = [
+//     {nombre: "ana", edad: 30},
+//     {nombre: "felipe", edad: 20}
+// ];
+
+// let joven = personas.find(p => p.edad < 25);
+// console.log(joven.nombre)
+
+//EJERCICIO AÑO DE NACIMIENTO------------
+
+// let personas = [
+//     {nombre: "Juan", edad: 19},
+//     {nombre: "Mario", edad: 22}
+// ];
+
+personas = personas.map(persona => ({
+    ...p,
+    habilidades: ["json", "htrml"]
+}))
+
+// function nacimiento(personas) {
+//     const anioActual = new Date().getFullYear();
+//     return personas.map(persona => ({
+//         ...persona, nacimiento: anioActual - persona.edad //...persona hace una copia de todas las propiedades del objeto
+//     }))
+// }
+
+// console.log(nacimiento(personas))
+
 let persona = {
-    nombre: "Agus",
-    edad: 31,
-    personaje: "Power ranger rosa",
-    saludar: function () {
-        console.log("Hola soy ", this.nombre)
-    }
+    nombre: "Mario",
+    edad: 33
 }
 
-persona.saludar()
+persona.edad = 31
+persona.habilidades = ["HTML", "CSS"]
+
+console.log(persona)
+
+let personaVacia = {}
+
+personaVacia.nombre = prompt("INgresa tu nombrew")
---
+++ Clase13_DOM/script.js
@@ -0,0 +1,4 @@
+console.log(document)
+
+let titulo = document.getElementById("titulo");
+titulo.textContent = "MOdificado por el DOMs"
