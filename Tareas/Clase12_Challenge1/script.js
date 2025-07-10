9c86214f0808ebe4c4f224bd8265f6f646e507af
angela-baltierrez <angelabaltierrez@gmail.com>
2025-07-10 17:03:07 -0300

    termino TP final

--- Tareas/Clase12_Challenge1/script.js
+++ Tareas/Clase12_Challenge1/script.js
@@ -1,88 +1,88 @@
-// function examinar(nota) {
-//     switch (true) {
-//         case nota >= 0 && nota < 2:
-//             return "Muy mal";
-//         case nota >= 2 && nota < 5:
-//             return "Mal";
-//         case nota >= 5 && nota < 6:
-//             return "Tan cerca pero tan lejos";
-//         case nota >= 6 && nota < 8:
-//             return "¡Bien!";
-//         case nota >= 8 && nota <= 10:
-//             return "¡¡Muy bien!!";
-//         default:
-//             return "Nota inválida. Por favor, introduce un valor entre 0 y 10.";
-//     }
-// }
+function examinar(nota) {
+    switch (true) {
+        case nota >= 0 && nota < 2:
+            return "Muy mal";
+        case nota >= 2 && nota < 5:
+            return "Mal";
+        case nota >= 5 && nota < 6:
+            return "Tan cerca pero tan lejos";
+        case nota >= 6 && nota < 8:
+            return "¡Bien!";
+        case nota >= 8 && nota <= 10:
+            return "¡¡Muy bien!!";
+        default:
+            return "Nota inválida. Por favor, introduce un valor entre 0 y 10.";
+    }
+}
 
-// console.log(examinar(4))
+console.log(examinar(7))
 
-// DOS   -------------------
+//DOS-----------------
 
-// function biggestOne(num1, num2, palabra) {
-//     if (num1 === num2) {
-//         const primera = palabra[0];
-//         const ultima = palabra[palabra.length - 1]
-//         return primera + ultima;
-//     } else if (num1 > num2) {
-//         return num1;
-//     } else {
-//         return num2;
-//     }
-// }
+function biggestOne(num1, num2, palabra) {
+    if (num1 === num2) {
+        const primera = palabra[0];
+        const ultima = palabra[palabra.length - 1]
+        return primera + ultima;
+    } else if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
 
-//onsole.log(biggestOne(2,9,"jota"))
+console.log(biggestOne(8    ,3,"jota"))
 
-//TRES -------------
+//TRES--------------------------
 
-// let frutasYVerduras = [
-//     {fruta:"banana"}, {verdura:"apio"}, {fruta:"manzana"}, {fruta:"frutilla"},
-//     {verdura:"zanahoria"},{fruta:"kiwi"},{fruta:"sandia"},{fruta:"melon"},
-//     {verdura:"repollo"},{fruta:"mango"}
-// ]
+let frutasYVerduras = [
+    {fruta:"banana"}, {verdura:"apio"}, {fruta:"manzana"}, {fruta:"frutilla"},
+    {verdura:"zanahoria"},{fruta:"kiwi"},{fruta:"sandia"},{fruta:"melon"},
+    {verdura:"repollo"},{fruta:"mango"}
+]
 
-// function filtradoFrutas(frutasYVerduras) {
-//     let listaFruta = []
-//     for (const fruta of frutasYVerduras) {
-//         if (fruta.fruta) {
-//             listaFruta.push(fruta.fruta)
-//         }
-//     }
-//     return listaFruta;
-// }
+function filtradoFrutas(frutasYVerduras) {
+    let listaFruta = []
+    for (const fruta of frutasYVerduras) {
+        if (fruta.fruta) {
+            listaFruta.push(fruta.fruta)
+        }
+    }
+    return listaFruta;
+}
 
-// console.log(filtradoFrutas(frutasYVerduras))
+console.log(filtradoFrutas(frutasYVerduras))
 
-//CUATRO
+//CUATRO----------------
 
-// let unidades = [1, 2, 3, 4];
-// let gaseosas = ["cocacola", "sprite", "fanta", "seven up"];
+let unidades = [1, 2, 3, 4];
+let gaseosas = ["cocacola", "sprite", "fanta", "seven up"];
 
-// function dispenserGaseosas(gaseosas, unidades) {
-//     let despensa = {};
-//     for (let i = 0; i < gaseosas.length; i++) {
-//         despensa[gaseosas[i]] = unidades[i];
-//     }
-//     return despensa;
-// }
+function dispenserGaseosas(gaseosas, unidades) {
+    let despensa = {};
+    for (let i = 0; i < gaseosas.length; i++) {
+        despensa[gaseosas[i]] = unidades[i];
+    }
+    return despensa;
+}
 
-// let miStock = dispenserGaseosas(gaseosas, unidades);
+let miStock = dispenserGaseosas(gaseosas, unidades);
 
-// console.log("Cantidad de sprite:", miStock["sebas"]);
+console.log("Cantidad de sprite:", miStock["sprite"]);
 
-//CINCO ---------------------------
+//CINCO ---------------------
 
-// let personas = [
-//     {nombre: "Juan", edad: 19},
-//     {nombre: "Mario", edad: 22}
-// ];
+let personas = [
+    {nombre: "Juan", edad: 19},
+    {nombre: "Mario", edad: 22}
+];
 
-// function nacimiento(personas) {
-//     const anioActual = new Date().getFullYear();
-//     return personas.map(persona => ({
-//         ...persona, nacimiento: anioActual - persona.edad //...persona hace una copia de todas las propiedades del objeto
-//     }))
-// }
+function nacimiento(personas) {
+    const anioActual = new Date().getFullYear();
+    return personas.map(persona => ({
+        ...persona, nacimiento: anioActual - persona.edad //...persona hace una copia de todas las propiedades del objeto
+    }))
+}
 
-// console.log(nacimiento(personas))
+console.log(nacimiento(personas))
--- Tareas/Clase17_JsFinal/script.js
+++ Tareas/Clase17_JsFinal/script.js
@@ -30,10 +30,46 @@ const addToDOM = () => {
     randomWord.textContent = `${palabraAleatoria}`
 }
 
+const actualizarTiempo = () =>{
+    timereal = setInterval(() => {
+        time--;
+        timeSpan.innerText = time + "s"
+        if (time === 0) {
+            clearInterval(timereal)
+            gameOver()
+        }
+    }, 1000)
+}
+
+const updateScore = () => {
+    score++
+    scoreHtml.innerText = score
+}
+
+const gameOver = () => {
+    let h1 = document.createElement("h2")
+    h1.textContent = "GAME OVER"
+    end_game_container.appendChild(h1)
+    let parrafo = document.createElement("p")
+    parrafo.textContent = `Score final: ${score}`
+    end_game_container.appendChild(parrafo)
+    let boton = document.createElement("button")
+    boton.textContent = "Volver a empezar"
+    boton.onclick = () => {
+        location.reload()
+    }
+    end_game_container.appendChild(boton)
+    main.remove()
+}
 
 let palabraAleatoria = randomWords()
 let time = 10
 let score = 0
+
+const main = document.querySelector(".main")
+const scoreHtml = document.querySelector("#score")
+const timeSpan = document.querySelector("#timeSpan")
+const end_game_container = document.querySelector("#end-game-container")
 
 addToDOM()
 
@@ -43,13 +79,14 @@ text.addEventListener("keyup", (e) => {
         const palabraIngresada = text.value;
         if (palabraIngresada === palabraAleatoria) {
             time += 3
-            console.log(text.value)
             text.value = ""
             palabraAleatoria = randomWords()
             addToDOM()
+            updateScore()
         } else {
             text.value = ""
         }
-        console.log(time)
     }
 })
+
+actualizarTiempo()
