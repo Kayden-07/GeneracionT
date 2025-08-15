0bec3673ffca6dcc35be70539d1119739ac9fcdb
Gino Mazzaglia <mazzagliagino@gmail.com>
2025-08-15 17:46:15 -0300

    termino tarea de vacaciones

--- Tareas/Clase19_PaVacaciones/script.js
+++ Tareas/Clase19_PaVacaciones/script.js
@@ -1,25 +1,78 @@
-colors = [
-    "rgb(255, 99, 71)",
-    "rgb(60, 179, 113)",
-    "rgb(30, 144, 255)",
-    "rgb(255, 215, 0)",
-    "rgb(138, 43, 226)",
-    "rgb(255, 105, 180)"
-];
 
-let squares = document.querySelectorAll(".square");
+function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function generateRandomColors(num) {
+    let ranColor = [];
+
+    for (let i = 0; i < num; i++) {
+        let color = randomColor();
+        ranColor.push(color);
+    }
+
+    return ranColor;
+}
 
 function pickColor() {
     let indiceRandom = Math.floor(Math.random() * colors.length);
     return colors[indiceRandom];
 }
 
+const dificultadDefault = 6; 
+let currentNumColors = dificultadDefault; 
+
+function dificultad(numColors) {
+    currentNumColors = numColors; // Actualiza el valor global
+    colors = generateRandomColors(numColors);
+    pickedcolor = pickColor();
+    colorDisplay.textContent = pickedcolor;
+    message.textContent = "";
+    h1.style.backgroundColor = "rgb(0, 81, 135)";
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].hidden = false;
+        } else {
+            squares[i].hidden = true;
+        }
+    }
+}
+
+let squares = document.querySelectorAll(".square");
+let colors = generateRandomColors(6);
+
 let pickedcolor = pickColor();
 let colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedcolor;
 
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function () {
+    dificultad(currentNumColors); 
+    resetButton.textContent = "Nuevos Colores";
+    message.textContent = "";
+});
+
 let message = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+
+let easyButton = document.querySelector("#easy");
+let hardButton = document.querySelector("#hard");
+
+easyButton.addEventListener("click", function () {
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    dificultad(3);
+});
+
+hardButton.addEventListener("click", function () {
+    hardButton.classList.add("selected");
+    easyButton.classList.remove("selected");
+    dificultad(6);
+});
 
 function changeColors(color) {
     for (let i = 0; i < squares.length; i++) {
@@ -35,6 +88,7 @@ for (let i = 0; i < squares.length; i++) {
             message.textContent = "¡Correcto!";
             h1.style.backgroundColor = pickedcolor;
             changeColors(pickedcolor);
+            resetButton.textContent = "Play Again?";
         } else {
             this.style.backgroundColor = "rgb(196, 218, 250)";
             message.textContent = "Intentalo Nuevamente";
