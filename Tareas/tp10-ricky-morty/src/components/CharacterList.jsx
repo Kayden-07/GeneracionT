535ca92d09b9131562687b327de1979f00fe0275
Gino Mazzaglia <mazzagliagino@gmail.com>
2025-08-22 12:59:29 -0300

    termino tarea RickMorty

--- Tareas/tp10-ricky-morty/src/components/CharacterList.jsx
+++ Tareas/tp10-ricky-morty/src/components/CharacterList.jsx
@@ -1,15 +1,25 @@
 import { fetchRickMortyCharacters } from '../services/RickMortyServices';
-import React, { useEffect, useState } from 'react';
-import { Grid, Card, CardMedia, CardContent, CardActionArea, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Grid, Card, CardMedia, CardContent, CardActionArea, Typography, Button } from '@mui/material';
 
 function CharacterList({ onSelectCharacter }) {
     const [characters, setCharacters] = useState([]);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
 
     useEffect(() => {
-        fetchRickMortyCharacters()
-            .then(data => setCharacters(data))
-            .catch(error => console.error('Error fetching characters:', error));
-    }, []);
+        loadCharacters(page); // Cargar personajes al montar el componente o cambiar de página
+    }, [page]); 
+
+    const loadCharacters = async (page) => { // Función para cargar personajes
+        try {
+            const data = await fetchRickMortyCharacters(page);
+            setCharacters((prev) => [...prev, ...data.results]); // Agregar nuevos personajes a la lista existente
+            setHasMore(data.info.next !== null);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <>
@@ -18,7 +28,7 @@ function CharacterList({ onSelectCharacter }) {
             </Typography>
             <Grid container spacing={5} justifyContent="center">
                 {characters.map(character => (
-                    <Card variant="outlined">
+                    <Card variant="outlined" key={character.id} sx={{ width: 300}}>
                         <CardActionArea onClick={() => onSelectCharacter(character)}>
                             <CardMedia component="img" image={character.image} alt={character.name} />
                             <CardContent>
@@ -30,6 +40,11 @@ function CharacterList({ onSelectCharacter }) {
                     </Card>
                 ))}
             </Grid>
+            {hasMore && (
+                <Button onClick={() => setPage((prev) => prev + 1)} variant="contained" color='success' sx={{ mt: 2 }} fullWidth>
+                    <Typography variant="body1">Cargar más personajes</Typography>
+                </Button>
+            )}
         </>
     );
 }
--- Tareas/tp10-ricky-morty/src/services/RickMortyServices.jsx
+++ Tareas/tp10-ricky-morty/src/services/RickMortyServices.jsx
@@ -1,8 +1,10 @@
-export async function fetchRickMortyCharacters() {
-  const response = await fetch(`https://rickandmortyapi.com/api/character`)
+// api.js
+export async function fetchRickMortyCharacters(page = 1) {
+  const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
   if (!response.ok) {
-    throw new Error(`Error en la API: ${response.status} ${response.statusText}`)
+    throw new Error(`Error en la API: ${response.status} ${response.statusText}`);
   }
-    const data = await response.json()
-    return data.results;
+
+  const data = await response.json();
+  return data;
 }
